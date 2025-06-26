91c574c4c1ec3634f99ac8df30b75c25df44d92d
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-06-26 19:35:40 +0530

    error fixed

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import ToggleProvider from './contextApi/ToggleProvider.js'
+import ToggleProvider from './contextApi/ToggleProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
